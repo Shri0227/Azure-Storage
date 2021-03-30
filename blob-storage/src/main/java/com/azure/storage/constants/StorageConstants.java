e92127befa53a31696447dffc13416804cc0825b
Shrikant Jagdale <ShrikantJagdale1111@gmail.com>
2021-03-30 13:21:42 +0530

    Connection string

--- blob-storage/src/main/java/com/azure/storage/constants/StorageConstants.java
+++ blob-storage/src/main/java/com/azure/storage/constants/StorageConstants.java
@@ -2,13 +2,13 @@ package com.azure.storage.constants;
 
 public class StorageConstants {
 
-	public static final String STORAGE_ACCOUNT_NAME = "notificationsblobstorage";
+	public static final String STORAGE_ACCOUNT_NAME = "blobstorage";
 	
-	public static final String CONNECTION_STRING = "DefaultEndpointsProtocol=https;AccountName=notificationsblobstorage;AccountKey=AWKdCdXxB47dqpDnv81Yv25rnxXmrOLsGkaiGnRTH4enRdBGx1Jqv1ZkI7pJ5zOazXmygMiULXZa4A3QqUG99A==;EndpointSuffix=core.windows.net";
+	public static final String CONNECTION_STRING = "connection_string";
 	
 	public static final String CONTAINER_CREATE_URI = "https://%s.blob.core.windows.net/%s?restype=container";
 	
-	public static final String ACCESS_KEY = "AWKdCdXxB47dqpDnv81Yv25rnxXmrOLsGkaiGnRTH4enRdBGx1Jqv1ZkI7pJ5zOazXmygMiULXZa4A3QqUG99A==";
+	public static final String ACCESS_KEY = "access_key";
 			
 	
 }
